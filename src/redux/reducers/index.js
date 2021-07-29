0328b5232ef7f0d4e7cc87e6c0278715b4581dbb
Minh Tâm <85051074+minhtam-mdct@users.noreply.github.com>
2021-07-29 14:54:01 +0700

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,9 @@
 import { combineReducers } from "redux";
-import userReducer from "./user";
 import sanphamReducer from "./sanpham";
-import oantuxiReducer from "./oantuxi"
 
 const rootReducer = combineReducers({
     //combine child reducer
-    userReducer,
     sanphamReducer,
-    oantuxiReducer,
 });
 
 export default rootReducer;
